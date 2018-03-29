3380e9cd56a6b433694ffb84de5bffd114d33a7b
raphrmx <rmx.raph@gmail.com>
2018-03-29 14:33:48 +0200

    change path dist

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 exports.__esModule = true;
-__export(require("./src/xmpp-rmx-message"));
-__export(require("./src/xmpp-rmx-utils"));
-__export(require("./src/xmpp-websocket"));
+__export(require("./dist/src/xmpp-rmx-message"));
+__export(require("./dist/src/xmpp-rmx-utils"));
+__export(require("./dist/src/xmpp-websocket"));
